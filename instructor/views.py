e9a65685454ac7e1de1cbea2d327047130dde117
Jayasankar <jayasankarmanoj@gmail.com>
2025-01-27 12:33:20 +0530

    second commit

--- instructor/views.py
+++ instructor/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.views.generic import View
+from instructor.forms import InstructorCreateForm
 # Create your views here.
+
+
+class InstructorCreateView(View):
+
+    def get(self,request,*args,**kwargs):
+
+        form_instance=InstructorCreateForm()
+
+        return render(request,'instructor_register.html',{'form':form_instance})
