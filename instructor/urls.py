f459e7b8b8cd209dfbcd86a8b10a2a464c109f48
Jayasankar <jayasankarmanoj@gmail.com>
2025-01-28 11:39:18 +0530

    completed instructor registration

--- instructor/urls.py
+++ instructor/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+
 from instructor import views
 
 urlpatterns=[
 
-    path("register/",views.InstructorCreateView.as_view(),name='instructor-register')
-]
+    path("register/",views.InstructorCreateView.as_view(),name='instructor-register'),
+
+]   
--- instructor/views.py
+++ instructor/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
 from django.views.generic import View
+
 from instructor.forms import InstructorCreateForm
 # Create your views here.
 
@@ -11,3 +13,26 @@ class InstructorCreateView(View):
         form_instance=InstructorCreateForm()
 
         return render(request,'instructor_register.html',{'form':form_instance})
+    
+    def post(self,request,*args,**kwargs):
+
+        form_data=request.POST
+
+        form_instance=InstructorCreateForm(form_data)
+
+        if form_instance.is_valid():
+
+            form_instance.instance.role="Instructor"
+
+            form_instance.instance.is_superuser=True
+
+            form_instance.instance.is_staff=True
+
+            form_instance.save()
+
+            return redirect('instructor-register')
+
+        else:
+
+            return render(request,'instructor_register.html',{'form':form_instance})
+        
