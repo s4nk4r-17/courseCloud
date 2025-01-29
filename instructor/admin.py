7839b6f1edcc88ba0d0049709a6bb4ab5cd80e17
Jayasankar <jayasankarmanoj@gmail.com>
2025-01-29 11:22:58 +0530

    third commit

--- instructor/admin.py
+++ instructor/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
-from instructor.models import User
+
+from instructor.models import User,Category,Course,Module
 # Register your models here.
 
 admin.site.register(User)
+
+admin.site.register(Category)
+
+class CourseAdmin(admin.ModelAdmin):
+
+    exclude=('owner',)
+
+    def save_model(self, request, obj, form, change):
+
+        if not change:
+            obj.owner=request.user
+        return super().save_model(request, obj, form, change)
+
+
+
+admin.site.register(Course,CourseAdmin)
--- instructor/models.py
+++ instructor/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 from django.contrib.auth.models import AbstractUser
 
+from embed_video.fields import EmbedVideoField
+
 class User(AbstractUser):
 
     ROLE_CHOICES=[
@@ -32,3 +34,65 @@ def create_instructor_profile(sender,instance,created,**kwargs):
         InstructorProfile.objects.create(owner=instance)
 
 post_save.connect(create_instructor_profile,User)
+
+
+class Category(models.Model):
+
+    name=models.CharField(max_length=200,unique=True)
+
+    def __str__(self):
+        return self.name
+    
+class Course(models.Model):
+
+    title=models.CharField(max_length=200)
+
+    description=models.TextField()
+
+    price=models.DecimalField(decimal_places=2,max_digits=5)
+
+    owner=models.ForeignKey(User,on_delete=models.SET_NULL,related_name="courses",null=True)
+
+    is_free=models.BooleanField(default=False)
+
+    picture=models.ImageField(upload_to="courseimages",null=True,blank=True,default="courseimages/default.png")
+
+    thumbnail=EmbedVideoField()
+
+    category_objects=models.ManyToManyField(Category)
+    
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    
+class Module(models.Model):
+
+    title=models.CharField(max_length=200)
+
+    course_object=models.ForeignKey(Course,on_delete=models.CASCADE,related_name="modules")
+
+    order=models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
+    
+# Lesson
+# =>title
+# =>module_object(FK)
+
+class Lesson(models.Model):
+
+    title=models.CharField(max_length=200)
+
+    module_object=models.ForeignKey(Module,on_delete=models.CASCADE,related_name='lessons')
+
+    video=EmbedVideoField(null=True)
+
+    order=models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.module_object.title} + {self.title}"
